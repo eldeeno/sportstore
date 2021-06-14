e6b5f037c7d954e44cbd4f69a64c02ef802d4ee7
Shamsuddeen Abdulkadir <ibnabdulkadirb@gmail.com>
2021-06-14 22:52:42 +0100

    initial commit

---
+++ src/components/ProductList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div v-for="p in products" v-bind:key="p.id" class="card m-1 p-1 bg-light">
+      <h4>
+        {{p.name}}
+        <span class="badge rounded-pill bg-primary float-end">
+          {{ p.price | currency}}
+        </span>
+      </h4>
+      <div class="card-text bg-white p-1">{{ p.description }}</div>
+    </div>
+  </div>
+</template>
+<script>
+import { mapState } from "vuex";
+export default {
+  computed: {
+    ...mapState(["products"])
+  },
+  filters: {
+    currency(value) {
+      return new Intl.NumberFormat("en-NG",
+          { style: "currency", currency: "NGN" }).format(value);
+    }
+  }
+}
+</script>
